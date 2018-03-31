a005f8b5a120162e1413bc001dea618e9ace1f0f
Steven Childrey <steven.childrey36@yahoo.com>
2018-03-30 20:15:32 -0400

    Generate tournaments api

---
+++ api/controllers/TournamentsController.js
@@ -0,0 +1,11 @@
+/**
+ * TournamentsController
+ *
+ * @description :: Server-side logic for managing Tournaments
+ * @help        :: See http://sailsjs.org/#!/documentation/concepts/Controllers
+ */
+
+module.exports = {
+	
+};
+
