1c81c3e1136f8b0a1b01cab12cdcbd8284e2a469
Steven Childrey <steven.childrey36@yahoo.com>
2018-04-05 03:05:07 -0400

    Add api function for each platform tournaments

--- api/controllers/TournamentsController.js
+++ api/controllers/TournamentsController.js
@@ -6,6 +6,7 @@
  */
 
 module.exports = {
+    // Get all upcoming tournaments and return JSON
 	getUpcomingTournaments: function(req,res){
         Tournaments.find({completed:false}).exec(function(err, tournaments){
             if(err){
@@ -13,6 +14,33 @@ module.exports = {
             }
             res.json(tournaments);
         })
-    }
+    },
+    // Get all upcoming PS4 tournaments and return JSON
+    getUpcomingPS4: function(req,res){
+        Tournaments.find({completed:false, platform:'PS4'}).exec(function(err, tournaments){
+            if(err){
+                res.send(500, 'Database Error');
+            }
+            res.json(tournaments);
+        })
+    },
+    // Get all upcoming XB1 tournaments and return JSON
+    getUpcomingXB1: function(req,res){
+        Tournaments.find({completed:false, platform:'XB1'}).exec(function(err, tournaments){
+            if(err){
+                res.send(500, 'Database Error');
+            }
+            res.json(tournaments);
+        })
+    },
+    // Get all upcoming PC tournaments and return JSON
+    getUpcomingPC: function(req,res){
+        Tournaments.find({completed:false, platform:'PC'}).exec(function(err, tournaments){
+            if(err){
+                res.send(500, 'Database Error');
+            }
+            res.json(tournaments);
+        })
+    },
 };
 
