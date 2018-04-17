b6d0d1c8a059203d4cdbbecca1fe9121203fb9c5
Steven Childrey <steven.childrey36@yahoo.com>
2018-04-17 06:46:44 -0400

    Now able to create a team in a tournament

--- api/controllers/TeamsController.js
+++ api/controllers/TeamsController.js
@@ -8,11 +8,21 @@
 module.exports = {
 	createTeam: function(req,res){
         var tournamentID = req.params.tournamentID
+        // If user is NOT logged in
         if(typeof req.session.userName == 'undefined'){
-            sails.log("It is undefined");
+            res.redirect('/');
         }
+        // If user IS logged in
         else{
-            sails.log(tournamentID);
+            sails.log(tournamentID + " " + req.session.userName);
+            Teams.create({name:req.session.userName, tournament:tournamentID}).exec(function(err){
+                sails.log("Created a Team");
+                if(err){
+                    res.send(500, 'Database Error');
+                }
+                // console.log(dateTime);
+                res.redirect('/');
+            });
         }
     }
 };
