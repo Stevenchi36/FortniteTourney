ea97bce1f203fcb830c97c9c2ade480a0cceef0d
Steven Childrey <steven.childrey36@yahoo.com>
2018-04-22 20:23:03 -0400

    Sails generate api Matches

---
+++ api/controllers/MatchesController.js
@@ -0,0 +1,11 @@
+/**
+ * MatchesController
+ *
+ * @description :: Server-side logic for managing Matches
+ * @help        :: See http://sailsjs.org/#!/documentation/concepts/Controllers
+ */
+
+module.exports = {
+	
+};
+
